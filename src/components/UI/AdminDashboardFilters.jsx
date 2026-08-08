d725e139a60081b359c74a779c656cb2ffa52695
Ken Dawson <ken@kendawson.online>
2026-08-08 18:57:51 -0400

    tweaking sizing for pill component

--- src/components/UI/AdminDashboardFilters.jsx
+++ src/components/UI/AdminDashboardFilters.jsx
@@ -42,11 +42,17 @@ export function AdminDashboardFilters({
         {/* Center: Current Playlist (on top for small screens) */}
         <div className="col-12 col-md-4 order-1 order-md-2 mb-2 border border-1 border-dark bg-info-subtle rounded-pill">
           <div className="d-flex align-items-center flex-md-column flex-lg-row gap-2 px-2 py-1">
+            
             <div className="small flex-grow-1 text-center" style={{ minWidth: 0 }}>
               {playlistName && (
                 <>
-                  <span className="text-nowrap fw-bold">Current Playlist: </span>
-                  <span className="font-monospace">"{playlistName}"</span>
+                  <span className="d-md-block d-lg-inline text-nowrap fw-bold">
+                    Current Playlist:
+                  </span>
+                  <span className="d-md-block d-lg-inline font-monospace">
+                    <span className="d-none d-lg-inline"> </span>
+                    "{playlistName}"
+                  </span>
                 </>
               )}
             </div>
